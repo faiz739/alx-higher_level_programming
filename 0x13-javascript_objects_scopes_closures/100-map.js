8cf7a6bbfef72402161ac8950cb5c898f5894685
unknown <faithfulchekwas@gmail.com>
2023-03-14 20:17:30 +0100

    all task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
+
+const newList = list.map((val, idx) => val * idx);
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(newList);
